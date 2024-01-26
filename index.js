6b80fd2a8bfc6b23ac93b68e9ce816d3baa17a31
tushar <swamitusharjee2018@gmail.com>
2024-01-26 23:13:42 +0530

    setup view engine to ejs and layout file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const path = require("path");
 // ENV FILE ACCESS
 const dotenv = require("dotenv").config();
 // PORT
 const port = 8000;
+// EJS LAYOUT
+const expressEjsLayout = require("express-ejs-layouts");
 // SERVER
 const app = express();
+
+// VIEW ENGINE
+app.set("view engine", "ejs");
+app.set("views", path.join(path.resolve(), "views"));
+app.use(expressEjsLayout);
 app.listen(port, (err) => {
   if (err) {
     console.error(`error in server running `);
--- models/userSchema.js
+++ models/userSchema.js
@@ -8,4 +8,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 const User = mongoose.model("User", userSchema);
-module.exports=User;
+module.exports = User;
