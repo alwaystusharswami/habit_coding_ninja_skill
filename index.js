c82cb3ba5f7919d8a8fbb88702e3e2eaab38412c
tushar <swamitusharjee2018@gmail.com>
2024-01-27 21:17:10 +0530

    navbar with sass middle ware

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ const dotenv = require("dotenv").config();
 const port = 8000;
 // EJS LAYOUT
 const expressEjsLayout = require("express-ejs-layouts");
+// SASS MIDDLEWARE
+const sassMiddleware = require("node-sass-middleware");
 // SERVER
 const app = express();
+// MIDDLEWARE
+app.use(sassMiddleware({
+  src:'./assets/scss',
+  dest:'./assets/css',
+  debug:true,
+  outputStyle:'expanded',
+  prefix:'/css'
+}))
+app.use(express.static(path.join(path.resolve(), "assets")));
+
 
 // VIEW ENGINE
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "views"));
 app.use(expressEjsLayout);
+
+app.set("layout extractStyles", true);
 app.use("/", require("./routes"));
 app.listen(port, (err) => {
   if (err) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const homeController = require("../controller/home");
+router.get("/", homeController.home);
 router.use("/user", require("./user"));
 
 module.exports = router;
