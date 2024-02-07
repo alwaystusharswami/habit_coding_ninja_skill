2579324e0b9c1c39338cb085a390e2930752bdbc
tushar <swamitusharjee2018@gmail.com>
2024-02-07 23:25:31 +0530

    sign in design complete

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const userController = require("../controller/user");
 const passport = require("passport");
 
-router.get("/signup", userController.signUp);
-router.get("/signin", userController.signIn);
+router.get("/signUp", userController.signUp);
+router.get("/signIn", userController.signIn);
 router.get("/profile", userController.profile);
 router.post("/create", userController.createUser);
 router.post(
   "/createSession",
-  passport.authenticate("local", { failureRedirect: "/user/signin" }),
+  passport.authenticate("local", { failureRedirect: "/user/signIn" }),
   userController.createSession
 );
 
