c51dde7452372d60ef55c901e181d29a1a0a2d26
tushar <swamitusharjee2018@gmail.com>
2024-02-09 19:37:45 +0530

    task scheduled at mid night complete

--- config/node-cron.js
+++ config/node-cron.js
@@ -1,10 +1,27 @@
 const cron = require("node-cron");
 const Habit = require("../models/habitSchema");
-cron.schedule("0 0 * * *", () => {
+cron.schedule("0 0 * * *", async () => {
   // This function will be called every day at midnight
   console.log("Task scheduled for midnight.");
   // Call your function here
+  const habit = await Habit.find();
+  for (let i of habit) {
+    if (i.complete) {
+      updateTrue(i.id);
+    } else {
+      updateFalse(i.id);
+    }
+  }
 });
-async function dayComplete() {
-    await Habit.updateMany({$set:{complete:false,strike:0,totalDays:0,totalComplete:0,day:0}})
+async function updateTrue(id) {
+  await Habit.findByIdAndUpdate(id, {
+    $set: { complete: false },
+    $inc: { totalDays: 1 },
+  });
+}
+async function updateFalse(id) {
+  await Habit.findByIdAndUpdate(id, {
+    $set: { complete: false, day: 0 },
+    $inc: { totalDays: 1 },
+  });
 }
