b1cd13d8622449711d7025c74f34c7344cb7bbc5
tushar <swamitusharjee2018@gmail.com>
2024-02-11 00:21:16 +0530

    mid noght change complete

--- config/node-cron.js
+++ config/node-cron.js
@@ -1,4 +1,5 @@
 const cron = require("node-cron");
+const WeeklyDoneHabit = require("../models/weeklyHabit");
 const Habit = require("../models/habitSchema");
 cron.schedule("0 0 * * *", async () => {
   // This function will be called every day at midnight
@@ -6,6 +7,11 @@ cron.schedule("0 0 * * *", async () => {
   // Call your function here
   const habit = await Habit.find();
   for (let i of habit) {
+    await WeeklyDoneHabit.create({
+      Habit:i.id,
+      date:new Date(),
+      complete:false
+    });
     if (i.complete) {
       updateTrue(i.id);
     } else {
--- controller/home.js
+++ controller/home.js
@@ -1,8 +1,13 @@
 const Habit = require("../models/habitSchema");
+const WeeklyDoneHabit = require("../models/weeklyHabit");
 module.exports.home = async (req, res) => {
   try {
     if (req.user) {
       const habit = await Habit.find({ User: req.user.id });
+      for(i of habit){
+      let week = await WeeklyDoneHabit.find({Habit:i.id});
+      console.log(week[0].date.getDate());
+      }
       return res.render("home", { habit });
     }
     return res.render("home");
@@ -10,5 +15,3 @@ module.exports.home = async (req, res) => {
     console.error(error);
   }
 };
-
-
