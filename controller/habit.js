7f31352157c5edc863c2c6cd91fbaac2f2e6b359
tushar <swamitusharjee2018@gmail.com>
2024-02-10 22:25:26 +0530

    change in habit tracking

--- controller/habit.js
+++ controller/habit.js
@@ -3,7 +3,6 @@ module.exports.createHabit = async (req, res) => {
   const habit = await Habit.create({
     title: req.body.title,
     time: req.body.time,
-    repeat: req.body.repeat,
     strike: 0,
     totalDays: 0,
     totalComplete: 0,
--- models/habitSchema.js
+++ models/habitSchema.js
@@ -3,7 +3,6 @@ const habitSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     time: { type: String, required: true },
-    repeat: { type: String, required: true },
     strike: { type: Number, required: true },
     totalDays: { type: Number, required: true },
     totalComplete: { type: Number, required: true },
